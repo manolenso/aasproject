ed7aa635701f79ad063741dc380045e5a1a32c6d
manolenso <rlaurent@tvmag.com>
2013-04-23 17:42:26 +0200

    fichiers mysql

---
+++ reserv/inc/chemin.php
@@ -0,0 +1,3 @@
+
+<?php $chemin="www.testproject.manolenso.fr/assproject/reserv/"; ?><!-- save chemin dev local -->
+// <?php $chemin="http://localhost/public/assproject/reserv/"; ?><!-- save chemin test sur site web -->
